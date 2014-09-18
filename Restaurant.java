7f1d71450d6aba4e6184dbbc2f0358f9e80ec4a6
Innocent <innocentudeh@gmail.com>
2014-09-18 20:14:59 +0000

    isTableFree() added

--- Restaurant.java
+++ Restaurant.java
@@ -1,12 +1,18 @@
 public class Restaurant{
 
-	private name_restaurant;
+	private String name_restaurant;
 
 	private int no_of_tables = 5;
 
+	private int no_of_avaliable_tables;
+
 	boolean isTableFree()
 	{
-		if 
+		
+		if (no_of_avaliable_tables == 0)
+			return false;
+		else
+			return true;
 	}
 	
 }
